5d98b022ecaca89122d89f8c98583ef58a85f22b
Mahdi Ghaznavi <ghaznavi.mahdi@gmail.com>
2019-03-16 10:38:24 +0330

    hyper parameters

--- HP.py
+++ HP.py
@@ -3,6 +3,9 @@ BOOKS_METADATA = "/mnt/storage1/Data/gutenberg/API/metadata/books_metadata.json"
 FEATURES_METADATA = "/mnt/storage1/Data/gutenberg/API/metadata/features_metadata.json"
 BOOKS_DIR = "/mnt/storage1/Data/gutenberg/books/"
 PARAGRAPH_METADATA = "/mnt/storage1/Data/gutenberg/API/metadata/paragraphs.npy"
+TRAIN_PATH = "/mnt/storage1/Data/last-sentence-data/train"
+TEST_PATH = "/mnt/storage1/Data/last-sentence-data/test"
+DATA_PATH = "/mnt/storage1/Data/last-sentence-data/small"
 
 pre_keys = ["is_analysed", "sents_num", "words_num", "tokens_num", "has_dialogue", "whole_dialogue"]
 keys = ["global_id", "book_id", "local_id"] + pre_keys
